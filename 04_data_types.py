46061471cc991d1bf31db1fadca85a5cc922ec77
Sujal12-data <sujalguptaiimt@gmail.com>
2026-06-30 18:16:35 +0530

    Added string indexing and slicing examples

--- 04_data_types.py
+++ 04_data_types.py
@@ -10,6 +10,10 @@
  # Float- all the numbers including decimal points are called floats. for example, 1.1, 2.2, 3.3, 4.4, 5.5 are all floats.
  # complex- all the numbers including imaginary numbers are called complex numbers. for example, 1+2j, 2+3j, 3+4j are all complex numbers.
 
+import code
+from os import name
+
+
 a = 69      #integer example
 
 print(type(a))
@@ -48,6 +52,100 @@ print(type(A))
 
 """Strings and type conversions"""
 
+#String take more space than other data types like int float etc
+
+# this happens because string stores every character with thier own unicode
+
+#unicode is a universal character encoding standard that assigns a unique number to every character in every language in the world. this is done to ensure that every character is represented in a unique way and can be used in any language.
+
+
+# code example below;
+
+A = "A"
+
+print(ord(A)) 
+
+#output- 65
+
+"""NOTE"""
+   # ord() function se sirf ek character ka unicode milta hai. agar aapko ek string ka unicode chahiye to aapko har character ka unicode alag alag lena padega. 
+
+Sujal = "8"
+
+print(ord(Sujal))
+
+
+# now if we want to get unicode of a word or a sentence then we can use char() function to get the unicode of a word or a sentence. char() function takes an integer as an argument and returns the character that corresponds to that integer in the unicode table..
+# for example, if we want to get the unicode of the word "hello" then we can use char() function to get the unicode of each character in the word "hello" and then we can concatenate them to get the unicode of the word "hello".  
+
+
+name = "Sujal"
+for ch in name:
+    print(ch,ord(ch))
+
+#output-
+# S 83
+# u 117
+# j 106
+# a 97
+# l 108
+
+
+# ORD converts a character to its unicode value and CHR converts a unicode value to its character.
+
+
+
+"""String indexing"""
+
+
+
+a = "Sujal Gupta"
+print(a[3])
+
+
+#output- a
+
+# this is known as string indexing.
+# we can access it by its index number.
+# its starts fron 0 and goes upto n-1 where n is the length of the string.
+
+
+#example -
+S = "Sujal Gupta"
+print(S[6], S[-4])
+
+#output - G u 
+
+
+text = "Sujal Gupta"
+print(text[0:7])
+
+#output- Sujal G
+
+text = "i am learning python programming"
+print(text[0:12])
+
+#output- i am learning
+
+text = "i am new to in this world of programming"
+print(text[0:18])
+#output- i am new to in this
+
+
+text = "hey what are you doing?"
+print(text[0:3])
+#output- hey
+
+
+text = "hey what are you doing?"
+print(text[0:3:1])
+
+text = "hey how are you doing?"
+print(text[0:3:3])
+
+
+a = "Sujal Kumar Gupta"
+print(a[0:5])
 
 
 
