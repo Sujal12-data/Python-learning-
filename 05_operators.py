07b24af4696a4a5db5cf298d75a7f21875c3f82c
Sujal Gupta <sujalguptaiimt@gmail.com>
2026-09-07 17:42:04 +0530

    Refactor arithmetic operations in 05_operators.py
    Updated operators and added arithmetic operations.

--- 05_operators.py
+++ 05_operators.py
@@ -1,3 +1,14 @@
-a=20
-b=20
-print(a
+a=10
+b=10
+print(a+b) #add 
+
+print(b-a) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a//b) #floor division
+print(a**b) #exponentiation
+
+print(32%5) #modulus (just gives the remainder of the division operation)
+
+
+
